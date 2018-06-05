a9426e62b39a271d2d40b24578214ad5995fad22
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-05 23:54:21 +0530

    Create board.py

---
+++ board.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.penup();
+turtle.goto(-600,320);
+turtle.pendown();
+#turtle.hideturtle();
+turtle.shape('turtle');
+for i in range(2):
+     turtle.forward(1200);
+     turtle.right(90);
+     turtle.forward(640);
+     turtle.right(90);
+turtle.penup();
+turtle.goto(-480,200);
+turtle.pendown();
+for i in range(2):
+     turtle.forward(960);
+     turtle.right(90);
+     turtle.forward(400);
+     turtle.right(90);
