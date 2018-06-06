279619cbdd25640e0ac2af485ab7e8154cd3cc8f
Ieshaan Saxena <31623825+ieshaan12@users.noreply.github.com>
2018-06-06 18:58:36 +0530

    Update board.py
    The outline is ready.

--- board.py
+++ board.py
@@ -1,19 +1,62 @@
 import turtle
 turtle.penup();
-turtle.goto(-600,320);
+turtle.goto(-600,340);
 turtle.pendown();
 #turtle.hideturtle();
 turtle.shape('turtle');
 for i in range(2):
      turtle.forward(1200);
      turtle.right(90);
-     turtle.forward(640);
+     turtle.forward(680);
      turtle.right(90);
 turtle.penup();
-turtle.goto(-480,200);
+turtle.goto(-460,230);
 turtle.pendown();
 for i in range(2):
-     turtle.forward(960);
+     turtle.forward(920);
      turtle.right(90);
-     turtle.forward(400);
+     turtle.forward(460);
      turtle.right(90);
+turtle.left(90);
+for i in range (5):
+    turtle.forward(110);
+    turtle.right(90);
+    turtle.forward(102.22);
+    turtle.right(90);
+    turtle.forward(110);
+    turtle.left(90);
+    if(i!=4):
+        turtle.forward(102.22);
+        turtle.left(90);
+for i in range(5):
+    turtle.forward(140);
+    turtle.right(90);
+    turtle.forward(51.11);
+    turtle.right(90);
+    turtle.forward(140);
+    turtle.left(90);
+    if(i!=4):
+        turtle.forward(51.11);
+        turtle.left(90);
+for i in range (5):
+    turtle.forward(110);
+    turtle.right(90);
+    turtle.forward(102.22);
+    turtle.right(90);
+    turtle.forward(110);
+    turtle.left(90);
+    if(i!=4):
+        turtle.forward(102.22);
+        turtle.left(90);
+for i in range(5):
+    turtle.forward(140);
+    turtle.right(90);
+    turtle.forward(51.11);
+    turtle.right(90);
+    turtle.forward(140);
+    turtle.left(90);
+    if(i!=4):
+        turtle.forward(51.11);
+        turtle.left(90);
+turtle.exitonclick();  
+
